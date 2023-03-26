5cdbd03d9f48fee585019bf5d4326077b04f9181
Subrahmanya Bhat <subgb98@gmail.com>
2023-03-26 22:44:13 +0530

    initial commit

---
+++ index.html
@@ -0,0 +1,5 @@
+"use strict";
+const add = (a, b) => {
+    return (a + b).toString();
+};
+console.log(add(2, 3));
---
+++ src/sandbox.ts
@@ -0,0 +1,5 @@
+const add = (a:number,b:number):string=>{
+    return (a+b).toString();
+}
+
+console.log(add(2,3))
